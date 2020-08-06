e1901b32c424f9e5d7718161e51d390cc6b24ac1
Lucas Augusto de Castro Coelho <lucasaugustocastro24@gmail.com>
2020-08-06 10:28:10 -0300

    Backend finalizado

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,69 +1,20 @@
 import {Router} from 'express'
-import db from '../database/connection';
-import convertHourToMinutes from '../utils/convertHourToMinutes';
+import ClassesController from '../controllers/ClassesController';
+import ConnectionsController from '../controllers/ConnectionsController';
 
 const route = Router();
 
-
-interface ScheduleItem {
-  week_day: string;
-  from: string;
-  to: string;
-}
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 
-route.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
+route.get('/classes', classesController.index);
+route.post('/classes', classesController.create);
 
-  const trx = await db.transaction();
 
-  try {
-    const insertedUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-  
-    const user_id = insertedUsersIds[0]
-  
-  
-    const insertdClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-  
-    const class_id = insertdClassesIds[0];
-  
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-      };
-    })
-  
-    await trx('class_schedule').insert(classSchedule)
-  
-    await trx.commit();
-  
-    return response.status(201).send();
-  } catch (err) {
-    await trx.rollback();
-    return response.status(400).json({
-      error: 'Unexpectes error while crating new class'
-    })
-  }
-});
+route.post('/connections', connectionsController.create);
+
+route.get('/connections', connectionsController.index);
+
 
 export default route;
